c4588ffbc3e4ea9afa245185796ef85b2939bbc4
femstaryeup <olulai@yahoo.com>
2016-10-31 16:46:32 +0000

    Create Account
    Creates new account, with first name test, random email, random
    surname. Uses the faker gem

--- createAccount.rb
+++ createAccount.rb
@@ -19,16 +19,33 @@ end
 
 And(/^I supply my details$/) do
 
+  @browser.text_field(:id => 'FirstName_13').set 'Test'
+
+  #puts Lastname = Faker::Name.last_name
+  @browser.text_field(:id => 'LastName_13').set Faker::Name.last_name
+
+  #Emailaddress = Faker::Internet.free_email
+  @browser.text_field(:id => 'Email_13').set Faker::Internet.free_email
+
+  @browser.text_field(:id => 'Password_13').set 'quality1234'
+
+  @browser.checkbox(:xpath => ".//*[@id='ReadTerms']").set
+
+
+
 end
 
 
 
 And(/^I click create my account$/) do
+  @browser.button(:xpath => ".//*[@id='create-account-form']/button").click
+
 
 end
 
 
 
 Then(/^I should be able to view my newly created account$/) do
+  puts @browser.title == 'My Pages | Electrolux - NOT FOR COMMERCIAL USE'
 
 end
